002c2de8525a7c47b478fd6620c788bbd8625f0a
Maleesha Lakshani <malishalakshaniyapa@gmail.com>
2025-02-02 07:54:20 +0530

    Initial Commit

---
+++ src/main/java/lk/ijse/dep13/browser/controller/MainSceneController.java
@@ -0,0 +1,14 @@
+package lk.ijse.dep13.browser.controller;
+
+import javafx.application.Platform;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebView;
+
+public class MainSceneController {
+
+    public AnchorPane root;
+    public TextField txtAddress;
+    public WebView webDisplay;
+
+}

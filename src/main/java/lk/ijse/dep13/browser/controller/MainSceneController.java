1ad8971fd1a7c0c9a7cf2ffe589c20f19edf6a81
Maleesha Lakshani <malishalakshaniyapa@gmail.com>
2025-02-03 07:33:24 +0530

    Identification the URL

--- src/main/java/lk/ijse/dep13/browser/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep13/browser/controller/MainSceneController.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep13.browser.controller;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
@@ -11,4 +12,49 @@ public class MainSceneController {
     public TextField txtAddress;
     public WebView webDisplay;
 
+    public void initialize() {
+        txtAddress.focusedProperty().addListener( (observable, oldValue, newValue) -> {
+            if(newValue)Platform.runLater( txtAddress::selectAll );
+        } );
+    }
+
+    public void txtAddressOnAction(ActionEvent actionEvent) {
+        loadWebpage(txtAddress.getText());
+    }
+
+    private void loadWebpage(String url) {
+        int i = 0;
+        String protocol = null;
+        String host = null;
+        int port = 0;
+        String path = null;
+
+        //Get protocol
+        if((i = url.indexOf( "://" )) != -1){
+           protocol = url.substring( 0,i );
+        }else protocol = "http";
+
+        //Get host and port number
+        int j = url.indexOf( "/", i == -1 ? (i = 0) : (i =  i + 3));
+        host = j == -1 ? url.substring( i ) : url.substring( i, j );
+        int portIndex = host.indexOf( ":" );
+        if(portIndex != -1){
+            port = Integer.parseInt( host.substring( portIndex + 1 ) );
+            host = host.substring( 0, portIndex );
+        }else {
+            port = protocol.equalsIgnoreCase( "http" ) ? 80 : protocol.equalsIgnoreCase( "https" ) ? 443 : -1;
+        }
+
+        //Get path
+        if((j != -1) && (j != url.length() - 1)){
+            path = url.substring(j+1);
+        }
+        if(host.isBlank()|| port == -1){
+           throw new RuntimeException( "Invalid URL");
+        }
+        System.out.println("protocol = " + protocol);
+        System.out.println("host = " + host);
+        System.out.println("port = " + port);
+        System.out.println("path = " + path);
+    }
 }

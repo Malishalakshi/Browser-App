7c29d64a29b340efa15b9c40e8ed7cc6ccbcaabe
Maleesha Lakshani <malishalakshaniyapa@gmail.com>
2025-02-06 16:40:25 +0530

    Get host and port number

--- src/main/java/lk/ijse/dep13/browser/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep13/browser/controller/MainSceneController.java
@@ -30,11 +30,24 @@ public class MainSceneController {
     private void loadWebpage(String url)  {
         int i = 0;
         String protocol = null;
+        String host = null;
+        int port = -1;
 
         //Get protocol
         if((i = url.indexOf( "://" )) != -1){
             protocol = url.substring( 0,i );
         }else protocol = "http";
+
+        //Get host and port number
+        int j = url.indexOf( "/", i == -1 ? (i = 0) : (i =  i + 3));
+        host = j == -1 ? url.substring( i ) : url.substring( i, j );
+        int portIndex = host.indexOf( ":" );
+        if(portIndex != -1){
+            port = Integer.parseInt( host.substring( portIndex + 1 ) );
+            host = host.substring( 0, portIndex );
+        }else {
+            port = protocol.equalsIgnoreCase( "http" ) ? 80 : protocol.equalsIgnoreCase( "https" ) ? 443 : -1;
+        }
 
     }
 }
